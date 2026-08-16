8a06b76a0a2059fdc0e89cc8f029da6967110ed1
naveen saini <naveensaini6378@gmail.com>
2026-08-16 21:16:39 +0530

    add routes table

---
+++ terraform/routes_table.tf
@@ -0,0 +1,45 @@
+# Public Route Table
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name        = "log-monitoring-public-rt"
+    Project     = "logs_monitoring_system"
+    Environment = var.environment
+  }
+}
+
+# Private Route Table
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
+
+  tags = {
+    Name        = "log-monitoring-private-rt"
+    Project     = "logs_monitoring_system"
+    Environment = var.environment
+  }
+}
+
+# Public Subnet Associations (2 subnets)
+resource "aws_route_table_association" "public_assoc" {
+  count = 2
+
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+# Private Subnet Association
+resource "aws_route_table_association" "private_assoc" {
+  subnet_id      = aws_subnet.private.id
+  route_table_id = aws_route_table.private_rt.id
+}
